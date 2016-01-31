d717ddffe9f27a253863795ab91de618f3595d72
Konstantin Beyer <kbeyer@localhost.localdomain>
2016-01-31 16:59:56 +0100

    hw11

--- sg.cxx
+++ sg.cxx
@@ -7,6 +7,9 @@ using namespace std;
 //-----------------------------------
 typedef complex<double> cmplx;
 //-----------------------------------
+const double pi=M_PI;
+const cmplx im= cmplx(0,1.0);
+//-----------------------------------
 void init( cmplx* const psi0, const double alpha, const double lambda,
            const double dx, const double dt,
            const int Nx, const double xmin);
@@ -15,27 +18,37 @@ void init( cmplx* const psi0, const double alpha, const double lambda,
 void writeToFile(const cmplx* const v, const string s, const double dx,
          const int Nx, const double xmin, const double alpha,
          const double lambda, const double omega, const double t);
+
+
+void step(cmplx* const f1, cmplx* const f0,
+          const double dt, const double dx,
+          const double h, const double m, const double k,const double x, const int N);
 //-----------------------------------
 int main(){
 
-	const int Nx = ;
-	const double xmin = ;
-  const double xmax = ;
-	const double Tend = ;
-	const double dx = ;
-	const double dt =   ;
+	const double h=1, m=1;
+	const int Nx = 300;
+	const double xmin = -40;
+  const double xmax = 40;
+	const double Tend = 10*pi;
+	const double dx = (xmax-xmin)/(Nx-1);
+	const double dt = dx/10.0  ;
   double t = 0;
+	const double omega=0.2;
+	const double k=omega*omega*m;
+	const double alpha=pow(m*k/h,1.0/4.0);
 	const int Na = 10;
-	int Nk = int(Tend / Na / dt + 0.5);
+  int Nk = int(Tend / Na / dt + 0.5);
 
 	const double lambda = 10;
-  const double omega = 0.2;
 
   stringstream strm;
 
 	cmplx* psi0 = new cmplx[Nx];
+        cmplx* psi1 = new cmplx[Nx];
+        cmplx* temp=NULL;
 
-	init(psi0, alpha, lambda, dx, dt, Nx, xmin);
+	init(psi0,alpha,lambda,dx,dt,Nx,xmin);
 
 	writeToFile(psi0,"psi_0", dx,Nx,xmin, alpha, lambda, omega,t);
 
@@ -43,7 +56,8 @@ int main(){
 
 	for (int i = 1; i <= Na; i++) {
 		for (int j = 1; j <= Nk-1; j++) {
-
+            step(psi1,psi0,dt,dx,h,m,k,xmin,Nx);
+            temp=psi1; psi1=psi0; psi0=temp;
          t+=dt;
 		}
 		strm.str("");
@@ -52,10 +66,38 @@ int main(){
 	}
   cout << "t = " << t << endl;
 
+        delete[] psi0;
+        delete[] psi1;
 	return 0;
 }
 //-----------------------------------
+void step(cmplx* const f1, cmplx* const f0,
+          const double dt, const double dx,
+          const double h, const double m, const double k,const double x, const int N)
+{
 
+  cmplx* d=new cmplx[N];
+  cmplx* u=new cmplx[N];
+  cmplx* f0_prime=new cmplx[N];
+    //initial conditions
+  for(int i=0;i<N;i++) d[i] = 1.0 + im*(h*dt/2.0/m/dx/dx+dt/4.0/h*k*pow(x+i*dx,2));
+  for(int i=0;i<N;i++) u[i] = - im*h*dt/4.0/m/dx/dx;
+    //New vector conj(A)*f0
+  f0_prime[0] = conj(d[0])*f0[0]+conj(u[0])*f0[1];
+  for(int i=1;i<N-1;i++) f0_prime[i] = conj(u[i])*f0[i-1]+conj(d[i])*f0[i]+conj(u[i])*f0[i+1];
+  f0_prime[N-1]=conj(u[N-1])*f0[N-2]+conj(d[N-1])*f0[N-1];
+    //solve via backwards substitution
+ for(int i=1; i<N; i++){
+    d[i]-=u[i]/d[i-1]*u[i-1];
+    f0_prime[i]-=u[i]/d[i-1]*f0_prime[i-1];
+  }
+  f1[N-1]=f0_prime[N-1]/d[N-1];
+  for(int i=N-2; i>0; i--) f1[i]=(f0_prime[i]-u[i]*f1[i+1])/d[i];
+
+  delete[] d;
+  delete[] u;
+  delete[] f0_prime;
+}
 //-----------------------------------
 void writeToFile(const cmplx* const v, const string s, const double dx,
                  const int Nx, const double xmin, const double alpha,
